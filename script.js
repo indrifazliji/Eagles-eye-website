bcf6a7538202d706d39ae6876d82a87e93723180
indri fazliji <93227926+indrifazliji@users.noreply.github.com>
2024-09-10 23:25:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Display a message when a user submits the form
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    alert('Thank you for reaching out! We will get back to you shortly.');
+});
